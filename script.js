8a2ee40ea15bfcbc6309be812ab52d9cc3e6a6b1
DemonL0RD125 <89476871+DemonL0RD125@users.noreply.github.com>
2026-01-03 20:23:28 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function recordIncome(amount) {
-  let incomeHistory = JSON.parse(localStorage.getItem("incomeHistory")) || [];
-  incomeHistory.push({ date: new Date().toLocaleDateString(), amount: amount });
-  localStorage.setItem("incomeHistory", JSON.stringify(incomeHistory));
-}
